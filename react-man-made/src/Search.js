8cf79e7bb7df56352024f19d83c5ba0155bf1943
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 17:15:03 -0500

    Update Search.js

--- react-man-made/src/Search.js
+++ react-man-made/src/Search.js
@@ -1,26 +1,54 @@
 import React from 'react';
 
-const Search = (props) => {
-
-    async function setStates(e) {
-        const response = await fetch("http://api.airvisual.com/v2/states?country=" + e.target.value + "&key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713")
-        const body = await response.json();
-        body.map((state) => <option key={state.state} value={state.state}>{state.state}</option>)
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCountry: "Afghanistan",
+            states: [],
+            cities: [],
+        }
+        this.setStates = this.setStates.bind(this);
+        this.setCities = this.setCities.bind(this);
+        this.selectCity = this.selectCity.bind(this);
     }
 
-    return (
-        <form >
-            <select id="select-country" onChange={setStates}>
-                {props.countries.data.map((country) => <option key={country.country} value={country.country}>{country.country}</option>)}
-            </select>
-            <select id="select-state">
+    async setStates(e) {
+        let country = e.target.value;
+        const response = await fetch("http://api.airvisual.com/v2/states?country=" + country + "&key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+        const body = await response.json();
+        this.setState({selectedCountry: country, states: body.data});
+    }
 
-            </select>
-            <select id="select-city">
+    async setCities(e) {
+        let state = e.target.value
+        const response = await fetch("http://api.airvisual.com/v2/cities?state=" + state +
+         "&country=" + this.state.selectedCountry + "&key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+        const body = await response.json();
+        this.setState({selectedState: state, cities: body.data});
+    }
 
-            </select>
-        </form>
-    )
+    async selectCity(e) {
+        let city = e.target.value
+        this.setState({selectedCity: city})
+    }
+
+    render() {
+        return (
+            <form >
+                <select id="select-country" onChange={this.setStates}>
+                    {this.props.countries.data.map((country) => <option key={country.country} value={country.country}>{country.country}</option>)}
+                </select>
+                <select id="select-state" onChange={this.setCities}>
+                    {this.state.states.map((state) => <option key={state.state} value={state.state}>{state.state}</option>)}
+                </select>
+                <select id="select-city" onChange={this.selectCity}>
+                    {this.state.cities.map((city) => <option key={city.city} value={city.city}>{city.city}</option>)}
+                </select>
+                <button type="submit" onClick={this.props.setLocation}>Submit</button>
+            </form>
+        )
+    }
 }
 
 export default Search;
