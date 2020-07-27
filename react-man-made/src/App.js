270bae91fad431994c7ef182ab1936d7225e0b35
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 15:29:22 -0500

    Update App.js

--- react-man-made/src/App.js
+++ react-man-made/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import NavBar from './NavBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    isLoading: true,
+  };
+
+  async componentDidMount() {
+    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+    const body = await response.json();
+    const countries = await fetch("http://api.airvisual.com/v2/countries?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+    const countryBody = await countries.json();
+    this.setState({ currentLocation: body.data, countries: countryBody, isLoading: false });
+  }
+
+  populateCountries(countryArray) {
+  let selected = document.getElementById("select-country");
+  for (let i = 0; i < countryArray.length; i++) {
+    let opt = countryArray[i].country;
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selected.appendChild(el);
+  }
+}
+
+render() {
+
+
+  if (this.state.isLoading) {
+    return <p>Loading...</p>
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <NavBar data={this.state.data} countries={this.state.countries} />
+            HELLO
+          </header>
+        <body className="App-body">
+
+        </body>
+      </div>
+    )
+  }
+}
 }
 
 export default App;
