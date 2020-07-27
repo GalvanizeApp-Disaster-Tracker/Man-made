d0481c1bdc80418f77f86c324e99829f308704fb
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 08:44:53 -0500

    Create LocationRepository.java

---
+++ spr-man-made/src/main/java/com/example/sprmanmade/LocationRepository.java
@@ -0,0 +1,10 @@
+package com.example.sprmanmade;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LocationRepository extends CrudRepository<Location, Long> {
+
+
+
+}
