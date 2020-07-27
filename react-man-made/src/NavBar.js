c216d04e668f6eac59827cfa9cfd01e41cfe62f6
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 15:29:51 -0500

    Create NavBar.js

---
+++ react-man-made/src/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Search from "./Search"
+
+const NavBar = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar nav">
+                <div id="nav-options">
+                    <li className="nav-item active">
+                        <a href="#">GET CURRENT CITY AIR INDEX (HOME)</a>
+                        </li>
+                    <li className="nav-item">
+                        <a href="#">SEARCH SPECIFIC CITY AIR INDEX (SEARCH W/ BAR)</a>
+                        <Search />
+                        </li>
+                    <li className="nav-item">
+                        <a href="#">LOGO (IMAGE)</a>
+                        </li>
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar;
