8f76f30d9f5f5f79a1ea785226a53e7d75b20720
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 10:55:02 -0500

    db of aqi levels

--- spr-man-made/src/main/java/com/example/sprmanmade/AQIController.java
+++ spr-man-made/src/main/java/com/example/sprmanmade/AQIController.java
@@ -2,9 +2,10 @@ package com.example.sprmanmade;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import javax.websocket.server.PathParam;
 
 @RestController
 public class AQIController {
@@ -15,46 +16,15 @@ public class AQIController {
         this.repository = repository;
     }
 
-    @GetMapping("/")
-    public ArrayList<AQI> getAqi() {
-        ArrayList<AQI> aqi = new ArrayList<AQI>();
-//        AQI aqi1 = new AQI();
-//        aqi1.setLevel(50);
-//        aqi1.setColor("Green");
-//        aqi1.setConcern("Good");
-//        aqi1.setDescription("Air quality is satisfactory, and air pollution poses little or no risk.");
-//        aqi.add(aqi1);
-//        AQI aqi2 = new AQI();
-//        aqi2.setLevel(100);
-//        aqi2.setColor("Yellow");
-//        aqi2.setConcern("Moderate");
-//        aqi2.setDescription("Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.");
-//        aqi.add(aqi2);
-//        AQI aqi3 = new AQI();
-//        aqi3.setLevel(150);
-//        aqi3.setColor("Orange");
-//        aqi3.setConcern("Unhealthy for Sensitive Groups");
-//        aqi3.setDescription("Members of sensitive groups may experience health effects. The general public is less likely to be affected.");
-//        aqi.add(aqi3);
-//        AQI aqi4 = new AQI();
-//        aqi4.setLevel(200);
-//        aqi4.setColor("Red");
-//        aqi4.setConcern("Unhealthy");
-//        aqi4.setDescription("Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.");
-//        aqi.add(aqi4);
-//        AQI aqi5 = new AQI();
-//        aqi5.setLevel(300);
-//        aqi5.setColor("Purple");
-//        aqi5.setConcern("Very Unhealthy");
-//        aqi5.setDescription("Health alert: The risk of health effects is increased for everyone.");
-//        aqi.add(aqi5);
-//        AQI aqi6 = new AQI();
-//        aqi6.setLevel(1000);
-//        aqi6.setColor("Maroon");
-//        aqi6.setConcern("Hazardous");
-//        aqi6.setDescription("Health warning of emergency conditions: everyone is more likely to be affected.");
-//        aqi.add(aqi6);
+    // displays the 6 different air quality index levels
+    @GetMapping("/levels")
+    public Iterable<AQI> getAqiLevels() {
+        return this.repository.findAll();
+    }
 
-        return aqi;
+    // displays corresponding air quality index level based on input int
+    @GetMapping("/levels/{level}")
+    public AQI getAQIByLevel(@PathVariable("level") int level) {
+        return this.repository.getLevel(level);
     }
 }
--- spr-man-made/src/main/java/com/example/sprmanmade/AQIRepository.java
+++ spr-man-made/src/main/java/com/example/sprmanmade/AQIRepository.java
@@ -1,9 +1,10 @@
 package com.example.sprmanmade;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AQIRepository extends CrudRepository<AQI, Long> {
 
-
-
+    @Query(value = "SELECT * FROM aqi_rankings WHERE min_level <= :level AND max_level >= :level", nativeQuery = true)
+    AQI getLevel(int level);
 }
