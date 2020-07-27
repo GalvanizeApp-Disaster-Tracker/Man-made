c54ac5d33a78f6d6ec6ebca2c51c06c08f5450f1
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 08:27:05 -0500

    Spring app

---
+++ spr-man-made/src/main/java/com/example/sprmanmade/SprManMadeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprmanmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprManMadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprManMadeApplication.class, args);
+	}
+
+}
---
+++ spr-man-made/src/test/java/com/example/sprmanmade/SprManMadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprmanmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprManMadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
