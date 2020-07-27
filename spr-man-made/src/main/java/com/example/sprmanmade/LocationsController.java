1c39133effbe843ade82a8bc4ccdd6e1e992a84e
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-27 08:50:56 -0500

    Update LocationsController.java

--- spr-man-made/src/main/java/com/example/sprmanmade/LocationsController.java
+++ spr-man-made/src/main/java/com/example/sprmanmade/LocationsController.java
@@ -1,4 +1,14 @@
 package com.example.sprmanmade;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LocationsController {
+
+    private final LocationRepository repository;
+
+    public LocationsController(LocationRepository repository) {
+        this.repository = repository;
+    }
+
 }
