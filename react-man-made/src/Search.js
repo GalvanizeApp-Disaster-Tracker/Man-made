a9de6f146e1d6a6b6d61aecc766fe4051e1196fa
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-28 07:44:18 -0500

    Update Search.js

--- react-man-made/src/Search.js
+++ react-man-made/src/Search.js
@@ -37,12 +37,15 @@ class Search extends React.Component {
         return (
             <form >
                 <select id="select-country" onChange={this.setStates}>
+                    <option disabled selected value>--Choose a country--</option>
                     {this.props.countries.data.map((country) => <option key={country.country} value={country.country}>{country.country}</option>)}
                 </select>
                 <select id="select-state" onChange={this.setCities}>
+                    <option disabled selected value>--Choose a state--</option>
                     {this.state.states.map((state) => <option key={state.state} value={state.state}>{state.state}</option>)}
                 </select>
                 <select id="select-city" onChange={this.selectCity}>
+                    <option disabled selected value>--Choose a city--</option>
                     {this.state.cities.map((city) => <option key={city.city} value={city.city}>{city.city}</option>)}
                 </select>
                 <button type="submit" onClick={this.props.setLocation}>Submit</button>
