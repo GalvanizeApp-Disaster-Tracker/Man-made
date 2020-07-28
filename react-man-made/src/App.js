ee909e07ed60315431d4188454fa6151384541c8
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-28 09:41:22 -0500

    Update App.js

--- react-man-made/src/App.js
+++ react-man-made/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar'
+import AQIData from './AQIData'
 
 
 class App extends Component {
@@ -10,20 +11,29 @@ class App extends Component {
       isLoading: true,
     };
     this.setCurrentLocation = this.setCurrentLocation.bind(this);
+    this.setNearestLocation = this.setNearestLocation.bind(this);
   }
 
   async componentDidMount() {
-    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
-    const body = await response.json();
+    // const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+    // const body = await response.json();
+    await this.setNearestLocation();
     const countries = await fetch("http://api.airvisual.com/v2/countries?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
     const countryBody = await countries.json();
-    this.setState({ currentLocation: body.data, countries: countryBody, isLoading: false });
+    this.setState({ countries: countryBody, isLoading: false });
+    // this.setState({ currentLocation: body.data, countries: countryBody, isLoading: false });
   }
 
-  async setCurrentLocation(country, city, state) {
+  async setCurrentLocation(city, state, country) {
     const response = await fetch("http://api.airvisual.com/v2/city?city=" + city + "&state=" + state + "&country=" + country + "&key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
     const body = await response.json();
     this.setState({currentLocation: body.data});
+  }
+
+  async setNearestLocation() {
+    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+    const body = await response.json();
+    this.setState({currentLocation: body.data})
   }
 
 render() {
@@ -35,9 +45,10 @@ render() {
     return (
       <div className="App">
         <header className="App-header">
-          <NavBar countries={this.state.countries} setLocation={this.setCurrentLocation}/>
+          <NavBar countries={this.state.countries} setLocation={this.setCurrentLocation} setNearestLocation={this.setNearestLocation}/>
             HELLO
           </header>
+          <AQIData currentLocation={this.state.currentLocation} aqius={this.state.currentLocation.current.pollution.aqius}/>
       </div>
     )
   }
