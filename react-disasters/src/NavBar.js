78d0c00d8f8943d2aa8d6670e05e9e6b77642eee
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-29 09:08:27 -0500

    Update NavBar.js

--- react-disasters/src/NavBar.js
+++ react-disasters/src/NavBar.js
@@ -4,13 +4,10 @@ import logo from './aqi.jpg'
 
 const NavBar = (props) => {
     return (
-        <nav>
-
-            <button>Home</button>
-            <Search countries={props.countries} apiKey={props.apiKey} 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button type="button" class="btn btn-light" onClick={() => props.setCurrentLocation(props.nearestLocation)}>Home</button>
+            <Search countries={props.countries} apiKey={props.apiKey}
                 setCurrentLocation={props.setCurrentLocation} />
-            <img src={logo} alt="logo"></img>
-
         </nav>
     )
 }
