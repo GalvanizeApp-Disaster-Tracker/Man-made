c9746f7b9a816e4f82e85a7e53d8efc5b3af4564
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-29 14:00:59 -0500

    Update Article.js

--- react-disasters/src/Article.js
+++ react-disasters/src/Article.js
@@ -6,10 +6,7 @@ const Article = (props) => {
         <div className="article-div">
             <img src={props.data.image} alt={props.data.message} className="article-img"></img>
             <div className="article-body">
-            <p>{props.data.articleDate}, {props.data.location}</p>
-            <br />
-            <p>{props.data.message}</p>
-            <br />
+            <h3>{props.data.message}</h3><p>{props.data.articleDate}, {props.data.location}</p>
             <p>{props.data.summary}</p><a href={props.data.articleLink} target="_blank">Read More...</a>
             </div>
         </div>
