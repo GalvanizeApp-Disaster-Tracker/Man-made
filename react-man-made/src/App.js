fb5cfa14dbaa1aa8ebf1c7d38e010042dd093b70
Alexis Kreager <alexisdkreager@gmail.com>
2020-07-29 07:39:39 -0500

    Update App.js

--- react-man-made/src/App.js
+++ react-man-made/src/App.js
@@ -19,14 +19,8 @@ class App extends Component {
     await this.setNearestLocation();
     // await this.setClosestLocation();
     // this.setState({isLoading: false})
-    const countries = await fetch("http://api.airvisual.com/v2/countries?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713", {
-      method: 'GET',
-      mode: 'no-cors',
-      cache: 'default',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
+    //https://pokeapi.co/api/v2/pokemon/ditto
+    //const countries = await fetch("http://api.airvisual.com/v2/countries?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
     const countryBody = await countries.json();
     await this.setState({ countries: countryBody, isLoading: false });
   }
@@ -59,24 +53,17 @@ class App extends Component {
   // }
 
   async setNearestLocation() {
-    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713", {
-      method: 'GET',
-      mode: 'no-cors',
-      cache: 'default',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    // const body = await response.json();
+    const response = await fetch("http://api.airvisual.com/v2/nearest_city?key=2dfa7b58-3bfb-4ec7-bddc-6cf7ec9b1713");
+    const body = await response.json();
     await this.setState({ currentLocation: response.json() });
   }
 
   render() {
 
-    if (this.state.isLoading) {
-      return <p>Loading...</p>
-    }
-    else {
+    // if (this.state.isLoading) {
+    //   return <p>Loading...</p>
+    // }
+    // else {
       return (
         <div className="App">
           <header className="App-header">
@@ -85,7 +72,7 @@ class App extends Component {
           <AQIData currentLocation={this.state.currentLocation} aqius={this.state.currentLocation.current.pollution.aqius} />
         </div>
       )
-    }
+    // }
   }
 }
 
